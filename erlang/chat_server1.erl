302a11f19bfabee2ecfe188e7de0a4c24df73bd7
Evan Liu <hmisty@gmail.com>
2012-05-26 08:23:35 +0000

    use register pid instead of ets

--- erlang/chat_server1.erl
+++ erlang/chat_server1.erl
@@ -1,9 +1,9 @@
-%% hmisty 20120520
+%% hmisty
 %% usage:
 %% term 1:
 %%      $ erl
-%%      > c(chat_server1).
-%%      > chat_server1:start(4000).
+%%      > c(chat_server2).
+%%      > chat_server2:start(4000).
 %% term 2:
 %%      $ nc localhost 4000
 %%      id a
@@ -22,22 +22,19 @@ listen(Port) ->
     {ok, Listen} = gen_tcp:listen(Port,
         [binary, {packet, 0}, {reuseaddr, true},
             {nodelay, true}, {active, false}]),
-    TabIds = ets:new(ids, [public, set]),
-    accept(Listen, TabIds).
+    accept(Listen).
 
-accept(Listen, TabIds) ->
-    ets:insert(TabIds, {a,1}),
+accept(Listen) ->
     {ok, Socket} = gen_tcp:accept(Listen),
     io:format("server opened socket:~p~n", [Socket]),
-    spawn(fun() -> loop(Socket, TabIds) end),
-    accept(Listen, TabIds).
+    spawn(fun() -> loop(Socket) end),
+    accept(Listen).
 
-loop(Socket, TabIds) ->
-    ets:insert(TabIds, {b,1}),
+loop(Socket) ->
     PidWriter = spawn(fun() -> loop_writer(Socket) end),
     case gen_tcp:recv(Socket, 0) of
         {ok, Bytes} ->
-            Reply = handle(Bytes, TabIds, PidWriter),
+            Reply = handle(Bytes, PidWriter),
             Str = case Reply of
                 {ok, Msg} ->
                     "ok " ++ Msg;
@@ -48,7 +45,7 @@ loop(Socket, TabIds) ->
             end,
             StrBytes = list_to_binary(Str),
             gen_tcp:send(Socket, StrBytes),
-            loop(Socket, TabIds);
+            loop(Socket);
         {error, closed} ->
             io:format("socket closed. exit.~n")
     end.
@@ -60,8 +57,7 @@ loop_writer(Socket) ->
             loop_writer(Socket)
     end.
 
-handle(Bytes, TabIds, PidWriter) ->
-    ets:insert(TabIds, {c,1}),
+handle(Bytes, PidWriter) ->
     %% Bytes is not marshalled term
     %% string is just a list of int()
     Str = binary_to_list(Bytes), 
@@ -70,21 +66,27 @@ handle(Bytes, TabIds, PidWriter) ->
     Reply = case Parts of 
         ["id",Id|_] ->
             io:format("got id:~p~n", [Id]),
-            io:format("insert {~p,~p} to ets table ~p~n", 
-                [Id, PidWriter, TabIds]),
-            ets:insert(TabIds, {Id, PidWriter}),
+            PidName = list_to_atom(Id),
+            case whereis(PidName) of
+                undefined ->
+                    register(PidName, PidWriter);
+                _ ->
+                    unregister(PidName),
+                    register(PidName, PidWriter)
+            end,
             {ok, "welcome, " ++ Id ++ ".\n"};
         ["m",From,To|Msgs] ->
             Msg = string:join(Msgs, " "),
             io:format("got msg:~p from:~p to:~p~n", [Msg, From, To]),
-            case ets:lookup(TabIds, To) of
-                [{To,PidTo}|_] ->
-                    io:format("ets lookup ~p successfully~n", [To]),
+            PidName = list_to_atom(To),
+            case whereis(PidName) of
+                undefined ->
+                    io:format("~p not found~n", [PidName]),
+                    {error, "sent failed, " ++ To ++ " is not online.\n"};
+                PidTo ->
+                    io:format("found ~p, send out ~p~n", [PidName, Bytes]),
                     PidTo ! Bytes,
-                    {ok, "successfully sent your msg: " ++ Msg ++ ".\n"};
-                _ ->
-                    io:format("ets lookup ~p failed~n", [To]),
-                    {error, "sent failed, " ++ To ++ " is not online.\n"}
+                    {ok, "successfully sent your msg: " ++ Msg ++ ".\n"}
             end;
         _ ->
             io:format("got unknown:~p~n", [Parts]),
