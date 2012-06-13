e42a9660ae8ab1667679f13e4be4d368c7ad7958
Evan Liu <hmisty@gmail.com>
2012-06-13 15:50:13 +0800

    create links sh

---
+++ home/create_links.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -s ~/coding/home/bash_aliases ~/.bash_aliases
+ln -s ~/coding/home/gitconfig ~/.gitconfig
+ln -s ~/coding/home/screenrc ~/.screenrc
+ln -s ~/coding/home/vimrc ~/.vimrc
