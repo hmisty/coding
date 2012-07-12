1b296edb4a3a8d6f96215f2948a4ed16c34969b3
Evan Liu <hmisty@gmail.com>
2012-07-13 00:05:41 +0800

    add displaying hello

--- lisp/square.scm
+++ lisp/square.scm
@@ -1,3 +1,13 @@
 #!gsi-script -:d0
 (define (main arg)
+  (display "hello")
+  (newline)
   (pretty-print (expt (string->number arg) 2)))
+;;; TODO 
+;;; known issue
+;;; $ gsi square 30
+;;; hello
+;;; 900
+;;; $ gsc -exe square
+;;; $ ./square 30
+;;; NO OUTPUT! BECAUSE main IS NOT CALLED!
