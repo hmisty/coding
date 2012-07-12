4715eb94b6e1b12cff7b121fe2254cd797f8a096
Evan Liu <hmisty@gmail.com>
2012-07-13 03:36:54 +0800

    makefile for linux

---
+++ lisp/m/m2.scm
@@ -0,0 +1,5 @@
+; File: "m2.scm"
+(c-declare "extern int power_of_2 ();")
+(define pow2 (c-lambda (int) int "power_of_2"))
+(define (twice x) (cons x x))
+
---
+++ lisp/m/m3.scm
@@ -0,0 +1,3 @@
+; File: "m3.scm"
+(write (map twice (map pow2 '(1 2 3 4)))) (newline)
+
