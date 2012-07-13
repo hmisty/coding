73dd321bc9901fb8054ce5dc38b884b57a7adb90
Evan Liu <hmisty@gmail.com>
2012-07-13 13:46:12 +0800

    try c-declare. cannot make using mingw

--- lisp/m/m2.scm
+++ lisp/m/m2.scm
@@ -1,5 +1,12 @@
 ; File: "m2.scm"
-(c-declare "extern int power_of_2 ();")
+;(c-declare "extern int power_of_2 ();")
+(c-declare #<<EOL
+
+extern int power_of_2 ();
+
+EOL
+)
+
 (define pow2 (c-lambda (int) int "power_of_2"))
 (define (twice x) (cons x x))
 
