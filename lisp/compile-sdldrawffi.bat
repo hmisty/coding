b5068d611058a34902f0322440743f10dc3a9172
Evan Liu <hmisty@gmail.com>
2012-07-15 07:36:30 +0800

    cmd /K to run bat is good

--- lisp/compile-sdldrawffi.bat
+++ lisp/compile-sdldrawffi.bat
@@ -1,3 +1,6 @@
+rem if exec under msys bash
+rem $ cmd /K XXX.bat
+rem
 rem gsc  -cc-options "-I/c/MinGW/include/SDL -IC:\\MinGW\\include\\SDL -IC:/MinGW/include/SDL -Dmain=SDL_main -D_GNU_SOURCE=1 -D_REENTRANT" -ld-options "-L/c/MinGW/lib -LC:\\MinGW\\lib -LC:/MinGW/lib -lmingw32 -mwindows -lSDLmain -lSDL -lSDL_draw"  -exe sdldrawffi.scm
 rem !!! -lSDL_draw MUST PRECEDE -lSDL !!!!! it sucks!
 gsc  -cc-options "-I/c/MinGW/include/SDL -IC:\\MinGW\\include\\SDL -IC:/MinGW/include/SDL -Dmain=SDL_main -D_GNU_SOURCE=1 -D_REENTRANT" -ld-options "-L/c/MinGW/lib -LC:\\MinGW\\lib -LC:/MinGW/lib -lSDL_draw -lSDLmain -lSDL"  -exe sdldrawffi.scm
