5118bb436c83a94ba9171911b0cec76e9e09d6e4
Evan Liu <hmisty@gmail.com>
2012-08-21 22:58:54 +0800

    a workable oozie minial example. dependency: oozie 3.1.3; hadoop 0.20.205.0

---
+++ hadoop/oozie-wordcount/src/main/java/app1/WordCountReducer.java
@@ -0,0 +1,25 @@
+package app1;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class WordCountReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+    // reduce(WritableComparable, Iterator, OutputCollector, Reporter)-
+    // Reducer has 3 primary phases: shuffle, sort and reduce.-
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+                       Reporter reporter) throws IOException {
+        int sum = 0;
+        while (values.hasNext()) {
+            sum += values.next().get();
+        }
+        output.collect(key, new IntWritable(sum));
+    }
+}
