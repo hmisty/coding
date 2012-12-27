5ca5f6f18ae22239f25b3f09ccda0abf779a43a6
Evan Liu <hmisty@gmail.com>
2012-12-27 23:21:23 +0800

    web app done

--- lisp/clojure/shorturl/src/shorturl/core.clj
+++ lisp/clojure/shorturl/src/shorturl/core.clj
@@ -1,6 +1,52 @@
-(ns shorturl.core)
+(ns shorturl.core
+  (:use [clojure.pprint]
+        [compojure.core :only (GET POST defroutes)])
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as resp]
+            [net.cgrand.enlive-html :as en]
+            [compojure.route]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+;; the counter algorithm
+(defonce counter (atom 10000))
+
+(defonce urls (atom {}))
+
+(defn shorten [url]
+  (let [id ( swap! counter inc )
+        id (Long/toString id 36 )]
+    (swap! urls assoc id url)) )
+
+;; the handlers
+#_(defn homepage [request]
+  (str @urls))
+
+(en/deftemplate homepage
+  (en/xml-resource "homepage.html" )
+  [request]
+  [:#listing :li] (en/clone-for [[id url] @urls]
+                                [:a] (comp
+                                        (en/content (format "%s : %s" id url))
+                                        (en/set-attr :href (str \/ id)))))
+
+(defn redirect [id]
+  (resp/redirect (@urls id)))
+
+;; the web app
+#_(defn app [request]
+  {:status 200
+   :body (with-out-str
+           (pprint request))})
+
+(defroutes app*
+  (compojure.route/resources "/")
+  (GET "/" request (homepage request))
+  (GET "/:id" [id] (redirect id))
+  (POST "/shorten" request
+        (let [id (shorten (-> request :params :url))]
+          (resp/redirect "/"))))
+
+;; the middleware
+(def app (compojure.handler/site app* ))
+
+;; the jetty server
+(defonce server (jetty/run-jetty #'app { :port 8081 :join? false}))
