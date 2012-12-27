f92753a2f516e93c4f6f4b7ce54cb58198adacbd
unknown <ehjlnox@.eapac.ericsson.se>
2012-12-27 22:58:23 +0800

    shorturl init

---
+++ lisp/clojure/shorturl/project.clj
@@ -0,0 +1,9 @@
+(defproject shorturl "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring "1.0.1"]
+                 [compojure "1.0.1"]
+                 [enlive "1.0.0"]])
