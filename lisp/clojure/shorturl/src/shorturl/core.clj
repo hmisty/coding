7a178ced42a6dcbc46d4ec56fd5ca0b686968752
Evan Liu <hmisty@gmail.com>
2012-12-29 23:30:43 +0800

    it works now with mvn package, jetty standalone and tomcat context-path!

--- lisp/clojure/shorturl/src/shorturl/core.clj
+++ lisp/clojure/shorturl/src/shorturl/core.clj
@@ -1,11 +1,16 @@
 (ns shorturl.core
+  (:import [javax.servlet.http HttpServletRequest HttpServletResponse])
+  (:gen-class :extends javax.servlet.http.HttpServlet)
   (:use [clojure.pprint]
-        [compojure.core :only (GET POST defroutes)])
+        [compojure.core :only (GET POST defroutes context)]
+        [ring.util.servlet :only (defservice servlet)]
+        #_[ring.middleware.params :only (wrap-params)])
   (:require [ring.adapter.jetty :as jetty]
             [ring.util.response :as resp]
             [net.cgrand.enlive-html :as en]
-             [compojure.handler]
-             [compojure.route]))
+            [compojure.handler]
+            [compojure.route]
+            [shorturl.context :as ctx]))
 
 ;; the counter algorithm
 (defonce counter (atom 10000))
@@ -28,27 +33,38 @@
   [:#listing :li] (en/clone-for [[id url] @urls]
                                 [:a] (comp
                                         (en/content (format "%s : %s" id url))
-                                        (en/set-attr :href (str \/ id)))))
+                                        (en/set-attr :href (ctx/link (str \/ id))))))
 
 (defn redirect [id]
   (resp/redirect (@urls id)))
 
-;; the web app
-#_(defn app [request]
+;; the handlers
+#_(defn handler [request]
   {:status 200
    :body (with-out-str
            (pprint request))})
 
-(defroutes app*
+(defroutes handler
   (compojure.route/resources "/")
   (GET "/" request (homepage request))
   (GET "/:id" [id] (redirect id))
   (POST "/shorten" request
         (let [id (shorten (-> request :params :url))]
-          (resp/redirect "/"))))
+          (resp/redirect (ctx/link "/"))))
+  ;; default route for avoiding: handler returns nil
+  (GET "*" request 
+       {:status 200 :body (with-out-str (pprint request))})
+)
 
-;; the middleware
-(def app (compojure.handler/site app* ))
+;; the web app by adding middleware
+(def app
+  (-> handler
+    (compojure.handler/site)
+    (ctx/wrap-context)))
+
+;; the servlet
+(defservice app)
 
 ;; the jetty server
-(defonce server (jetty/run-jetty #'app { :port 8081 :join? false}))
+#_(defonce server (jetty/run-jetty #'app {:port 8081 :join? false}))
+;; (.stop server) to stop
