4e475b9c014a828f7946755477c7194127c4f4c3
Evan Liu <hmisty@gmail.com>
2012-12-31 01:06:50 +0800

    rm fastodo

--- lisp/clojure/fastodo/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject fastodo "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]]
-            :main fastodo.server)
-
