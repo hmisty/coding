03d6c091cb990e065df6eb1a412e1e8fe4b36936
Evan Liu <hmisty@gmail.com>
2013-02-03 12:22:02 +0800

    add vim configs

--- home/create_links.sh
+++ home/create_links.sh
@@ -3,4 +3,10 @@
 ln -s ~/coding/home/bash_aliases ~/.bash_aliases
 ln -s ~/coding/home/gitconfig ~/.gitconfig
 ln -s ~/coding/home/screenrc ~/.screenrc
+
 ln -s ~/coding/home/vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+ln -s ~/coding/home/vim/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+ln -s ~/coding/home/vim/vim-sensible ~/.vim/bundle/vim-sensible
+ln -s ~/coding/home/vim/VimClojure ~/.vim/bundle/VimClojure
+ln -s ~/coding/home/vim/tabular ~/.vim/bundle/tabular
