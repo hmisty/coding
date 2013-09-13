9132e24128e9ad01e309fe50d50e4baababed392
Evan Liu <hmisty@gmail.com>
2013-09-13 10:44:20 +0800

    use vundle to maintain vim plugins

--- home/create_links.sh
+++ home/create_links.sh
@@ -6,18 +6,3 @@ ln -s ~/coding/home/screenrc ~/.screenrc
 
 ln -s ~/coding/home/vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-ln -s ~/coding/home/vim/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
-ln -s ~/coding/home/vim/vim-sensible ~/.vim/bundle/vim-sensible
-mkdir -p ~/.local/share/vim/undo ~/.local/share/vim/backup ~/.local/share/vim/swap
-#ln -s ~/coding/home/vim/VimClojure ~/.vim/bundle/VimClojure
-ln -s ~/coding/home/vim/tabular ~/.vim/bundle/tabular
-
-## replace foreplay with fireplace
-#ln -s ~/coding/home/vim/vim-foreplay ~/.vim/bundle/vim-foreplay
-ln -s ~/coding/home/vim/vim-fireplace ~/.vim/bundle/vim-fireplace
-
-## donot link classpath for using nrepl always. speed up the vim start speed
-#ln -s ~/coding/home/vim/vim-classpath ~/.vim/bundle/vim-classpath
-
-ln -s ~/coding/home/vim/vim-clojure-static ~/.vim/bundle/vim-clojure-static
-ln -s ~/coding/home/vim/rainbow_parentheses.vim ~/.vim/bundle/rainbow_parentheses.vim
