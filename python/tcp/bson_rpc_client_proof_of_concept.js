4b84f4d9424118f8ad57bbf1b241797f5cdfabf1
Evan Liu <hmisty@gmail.com>
2017-09-11 19:14:11 +0800

    benchmark works

--- python/tcp/bson_rpc_client_proof_of_concept.js
+++ python/tcp/bson_rpc_client_proof_of_concept.js
@@ -19,10 +19,10 @@ var doc = {
 	fn: 'add',
 	args: [1, 2],
 };
-var data = obj.serialize(doc);
-console.log('data: ', data);
+var rpc_data = obj.serialize(doc);
+console.log('data: ', rpc_data);
 
-var doc2 = obj.deserialize(data);
+var doc2 = obj.deserialize(rpc_data);
 console.log('doc2: ', doc2);
 
 var host = '127.0.0.1';
@@ -32,20 +32,42 @@ client.connect(port, host, () => {
 	console.log('connected');
 });
 
+var keep_running = true;
+var success = 0;
+var failure = 0;
+
 client.on('data', (data) => {
 	var obj = new bson();
 	var doc = obj.deserialize(data);
-	console.log('received doc: ', doc);
+	//console.log('received doc: ', doc);
+	if (doc.error_code == 0 && doc.result == 3) { //1+2=3
+		success += 1;
+	} else {
+		failure += 1;
+		console.log('error! ', doc);
+	}
+	
+	if (keep_running) {
+		client.write(rpc_data);
+	} else {
+		client.destroy();
+	}
 });
 
-client.on('close', () => {
-	console.log('conn closed');
-});
+var time_to_run = 5 * 1000; // 5 seconds
+var start = Date.now();
+setTimeout(() => {
+	keep_running = false;
 
-console.log('call remote fn: ' + doc.fn + ' with doc: ' + JSON.stringify(doc));
+	var elapsed = Date.now() - start;
+	console.log('Time elapsed: ' + elapsed + ' ms');
+	console.log('Successful requests: ' + success);
+	console.log('Failed requests: ' + failure);
+	console.log('Request per second: ' + Math.floor(success / elapsed * 1000));
+}, time_to_run); //stop after predefined seconds
 
-for (var i = 0; i < 1000; i++) {
-	client.write(data);
+var workers = 1; //20;
+for (var i = 0; i < workers; i++) {
+	client.write(rpc_data);
 }
 
-//client.destroy();
