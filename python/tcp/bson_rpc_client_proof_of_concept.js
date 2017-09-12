d88ec106bd60dde394479d3aa62928de55c1827f
Evan Liu <hmisty@gmail.com>
2017-09-12 23:04:24 +0800

    not finished

--- python/tcp/bson_rpc_client_proof_of_concept.js
+++ python/tcp/bson_rpc_client_proof_of_concept.js
@@ -1,6 +1,44 @@
 var bson = require('bson');
 var net = require('net');
 
+var Proxy = function(){
+	this.connection = null;
+};
+
+Proxy.prototype.connect = function() {
+};
+
+Proxy.prototype.use_service = function(names) {
+	var conn = this.connection;
+	for (var name in names) {
+		Proxy.prototype[name] = function() {
+			var args = Array.prototype.splice.call(arguments, 1, arguments.length);
+			var doc = {
+				fn: name,
+				args: args,
+			};
+			var rpc_data = obj.serialize(doc);
+			conn.write(rpc_data); //FIXME if conn is not connected
+		};
+	}
+};
+
+Proxy.prototype.on_result = function(callback) {
+	var conn = this.connection;
+	conn.on('data', (data) => {
+		var obj = new bson();
+		var doc = obj.deserialize(data);
+		if (doc.error_code == 0) {
+			callback(doc.result);
+		} else {
+			error(doc.error_code, doc.error_msg);
+		}
+	});
+};
+
+Proxy.prototype.disconnect = function() {
+};
+
 /*
 var bson_rpc = require('bson_rpc');
 
