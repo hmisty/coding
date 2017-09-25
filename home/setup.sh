ae700330f70db1686667207ae18af270d51d1eb7
Evan Liu <hmisty@gmail.com>
2017-09-25 18:09:01 +0800

    tmux.conf

--- home/setup.sh
+++ home/setup.sh
@@ -4,6 +4,8 @@ ln -s ~/coding/home/bash_aliases ~/.bash_aliases
 ln -s ~/coding/home/gitconfig ~/.gitconfig
 ln -s ~/coding/home/screenrc ~/.screenrc
 
+ln -s ~/coding/home/tmux.conf ~/.tmux.conf
+
 ln -s ~/coding/home/vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
