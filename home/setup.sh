597d5d409c4daec774eff220879710250af1709b
Evan Liu <hmisty@gmail.com>
2017-10-01 07:57:02 +0800

    two tmux configuration. red status bar for server ops for differentiation and caution reminder.

--- home/setup.sh
+++ home/setup.sh
@@ -4,7 +4,11 @@ ln -s ~/coding/home/bash_aliases ~/.bash_aliases
 ln -s ~/coding/home/gitconfig ~/.gitconfig
 ln -s ~/coding/home/screenrc ~/.screenrc
 
-ln -s ~/coding/home/tmux.conf ~/.tmux.conf
+if [[ `tty` =~ 'pts' ]]; then # 登陆终端是/dev/pts*，表示是远程登陆，而不是本地终端/dev/tty*
+	ln -s ~/coding/home/tmux.ops.conf ~/.tmux.conf
+else
+	ln -s ~/coding/home/tmux.dev.conf ~/.tmux.conf
+fi
 
 ln -s ~/coding/home/vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle
