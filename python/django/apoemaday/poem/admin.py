52e1052af1cfe825e51a12da7027ccefcd7f8341
Evan Liu <hmisty@gmail.com>
2017-12-07 18:04:25 +0800

    add search and list filter

--- python/django/apoemaday/poem/admin.py
+++ python/django/apoemaday/poem/admin.py
@@ -6,9 +6,13 @@ from .models import Author, Poem
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'pinyin')
+    search_fields = ('name', 'era',)
+    list_filter = ('era',)
 
 class PoemAdmin(admin.ModelAdmin):
     list_display = ('title', 'author',)
+    search_fields = ('title', 'author__name',)
+    list_filter = ('author__name', 'author__era',)
 
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Poem, PoemAdmin)
