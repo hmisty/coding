77c441536648ce5c3fdcf49f76996d31b116e4a3
Evan Liu <hmisty@gmail.com>
2019-12-02 11:53:34 +0800

    place order

--- python/ex_simulation/sim_place_order.py
+++ python/ex_simulation/sim_place_order.py
@@ -1,36 +1,90 @@
 #!/bin/env python3
+import math
 import random
+
+# price step
+PRICE_STEP = 0.01
 
 # max order amount
 MAX_AMOUNT = 10000
 
-# marker makers [(price: amount)]
+# marker makers [(price, amount)]
 buys = []
 sells = []
 
-# ticks
-for i in range(1000):
-    # round #i
-    print("round %d" % i)
+def place_order_buy(price, amount):
+    i = 0
+    for i in range(len(buys)):
+        if price >= buys[i][0]:
+            break
 
-    # place one buy order
-    buy1_price = buys[0][0] if len(buys) else 0
-    price_high = buy1_price * (1 + 0.01) # up 1%
-    price_low = max(0, buy1_price * (1 - 0.5)) # down 50%
-    price = (1 - random.random()) * (price_high - price_low) + price_low
-    amount = (1 - random.random()) * MAX_AMOUNT
+    if len(buys) > 0 and price == buys[i][0]:
+        buys[i] = (buys[i][0], buys[i][1] + amount)
+    elif len(buys) > 0 and price > buys[i][0]:
+        buys.insert(0, (price, amount))
+    else:
+        buys.insert(i+1, (price, amount))
 
-    print("place buy order at price %f with amount %f" % (price, amount))
-    #place_order_buy(price, amount)
+def place_order_sell(price, amount):
+    i = 0
+    for i in range(len(sells)):
+        if price <= sells[i][0]:
+            break
 
-    # place one sell order
-    sell1_price = sells[0][0] if len(sells) else 0
-    price_high = sell1_price * (1 + 0.5) # up 50%
-    price_low = max(0, sell1_price * (1 - 0.01)) # down 1%
-    price = (1 - random.random()) * (price_high - price_low) + price_low
-    amount = (1 - random.random()) * MAX_AMOUNT
+    if len(sells) > 0 and price == sells[i][0]:
+        sells[i] = (sells[i][0], sells[i][1] + amount)
+    elif len(sells) > 0 and price < sells[i][0]:
+        sells.insert(i, (price, amount))
+    else:
+        sells.append((price, amount))
 
-    print("place sell order at price %f with amount %f" % (price, amount))
-    #place_order_sell(price, amount)
 
+if __name__ == '__main__':
+    # ticks
+    for i in range(1000):
+        # round #i
+        print("round %d" % i)
+
+        # place one buy order
+        buy1_price = buys[0][0] if len(buys) else 0
+
+        if random.choice([True, False]):
+            price = buy1_price + PRICE_STEP # up 1 step
+        else:
+            price_high = buy1_price # down 0
+            price_low = max(0, buy1_price - 50 * PRICE_STEP) # down 50 step
+            price = random.random() * (price_high - price_low) + price_low
+
+        amount = (1 - random.random()) * MAX_AMOUNT
+
+        # set precision 0.01
+        price = math.floor(price * 100) / 100
+        amount = math.floor(amount * 100) / 100
+
+        print("place buy order at price %f with amount %f" % (price, amount))
+        place_order_buy(price, amount)
+
+        # place one sell order
+        sell1_price = sells[0][0] if len(sells) else 0
+
+        if random.choice([True, False]):
+            price = max(0, sell1_price - PRICE_STEP) # down 1 step
+        else:
+            price_high = sell1_price + 50 * PRICE_STEP # up 50 step
+            price_low = max(0, sell1_price) # up 0
+            price = random.random() * (price_high - price_low) + price_low
+
+        amount = (1 - random.random()) * MAX_AMOUNT
+
+        # set precision 0.01
+        price = math.floor(price * 100) / 100
+        amount = math.floor(amount * 100) / 100
+
+        print("place sell order at price %f with amount %f" % (price, amount))
+        place_order_sell(price, amount)
+
+
+    # inspect makers
+    print(buys)
+    print(sells)
 
