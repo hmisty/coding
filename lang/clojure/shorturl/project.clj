e6be35171016b2f6f2aa9de1dc3d76a1b5509b76
Evan Liu <hmisty@gmail.com>
2020-03-07 09:05:55 +0800

    upgrade versions

--- lang/clojure/shorturl/project.clj
+++ lang/clojure/shorturl/project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/hmisty/coding/lisp/clojure/shorturl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring "1.0.1"]
-                 [compojure "1.0.1"]
-                 [enlive "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.0"]
+                 [compojure "1.6.1"]
+                 [enlive "1.1.6"]]
 ;;  :aot [shorturl.server]
   :main shorturl.server
-  :plugins [[lein-ring "0.7.5"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler shorturl.core/app})
