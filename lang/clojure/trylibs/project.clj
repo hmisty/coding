c18864397cb9fb2ca05391d34f1ddd9d21785686
Evan Liu <hmisty@gmail.com>
2020-03-07 08:50:03 +0800

    use clojure 1.10.1

--- lang/clojure/trylibs/project.clj
+++ lang/clojure/trylibs/project.clj
@@ -1,4 +1,4 @@
 (defproject trylibs "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.ibdknox.socket_io_netty/socket-io-netty "0.3.2"]
                  [svm-clj "0.1.3"]])
