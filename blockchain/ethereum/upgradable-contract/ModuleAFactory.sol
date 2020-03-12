4d9bfc82a32107012ca326d94b7e225dbea38570
Evan Liu <hmisty@gmail.com>
2020-03-12 17:13:17 +0800

    the almost complete version.

--- blockchain/ethereum/upgradable-contract/ModuleAFactory.sol
+++ blockchain/ethereum/upgradable-contract/ModuleAFactory.sol
@@ -8,6 +8,11 @@ import "ModuleA.sol";
  */
 contract ModuleAFactory is managed {
 
+    // the current implementation
+    address public _implementation = 0x0;
+    // all available implementations
+    mapping(address => bool) _availableImplementations;
+
     /**
      * NOTICE: Let only the system manager deploy!
      * 
@@ -17,7 +22,7 @@ contract ModuleAFactory is managed {
     }
     
     /**
-     * Only manager can create a new module, just like
+     * Anyone can create a new module, just like
      * to register a new company at the government.
      */
     function create() public returns (address) {
@@ -25,9 +30,15 @@ contract ModuleAFactory is managed {
     }
     
     function createFor(address owner) public returns (address) {
+        require(_implementation != 0x0, "no implementation ready to use.");
+        
         ModuleA _newModule = new ModuleA(); // its msg.sender will be the factory
         _newModule.setupStorage(0x0);
         _newModule.setupOwner(owner);
+        
+        // automatically associate the current implementation for the module just created.
+        _newModule.changeImplementation(_implementation);
+        
         return _newModule;
     }
 
@@ -36,16 +47,27 @@ contract ModuleAFactory is managed {
      */
     function createFrom(address _legacyModule) onlyManager public returns (address) {
         require(_legacyModule != 0x0, "legacy module not exists.");
+        
+        // retrieve important data from the legacy module.
         ModuleA _oldModule = ModuleA(_legacyModule);
         
         address owner = _oldModule.getOwner();
         address stor = _oldModule.getStorage();
-        require(owner != 0x0, "legacy module has no owner");
-        require(stor != 0x0, "legacy module has no storage");
+        address impl = _oldModule.implementation();
+        require(owner != 0x0, "legacy module has no owner.");
+        require(stor != 0x0, "legacy module has no storage.");
+        require(impl != 0x0, "legacy module has no implementation.");
         
+        // create the new module.
         ModuleA _newModule = new ModuleA(); // its msg.sender will be the factory
         _newModule.setupStorage(stor);
         //_newModule.setupOwner(owner); // already in the storage, no need to transfer again
+        
+        // automatically inherit the legacy implementataion.
+        // NOTICE: we won't register the legacy implementataion automatically,
+        // for it's neither mandatory nor good.
+        _newModule.changeImplementation(impl);
+
         return _newModule;
     }
     
@@ -76,11 +98,50 @@ contract ModuleAFactory is managed {
     }
     
     /**
+     * Only manager can set the current implementation address.
+     */
+    function setCurrentImplementation(address _newImpl) onlyManager public {
+        require(_newImpl != 0x0, "implementation not exists.");
+        require(_newImpl != _implementation, "already the current implementation.");
+        // register it first if not yet.
+        if (_availableImplementations[_newImpl] == false) {
+            registerImplementation(_newImpl);
+        }
+        // then set it to the current implementation.
+        _implementation = _newImpl;
+    }
+ 
+    /**
+     * Only manager can register/unregister a new implementation address.
+     */
+    function registerImplementation(address _newImpl) onlyManager public {
+        require(_newImpl != 0x0, "implementation not exists.");
+        require(_availableImplementations[_newImpl] == false, "already registered.");
+        _availableImplementations[_newImpl] = true;
+    }
+    
+    function unregisterImplementation(address _newImpl) onlyManager public {
+        require(_newImpl != 0x0, "implementation not exists.");
+        require(_availableImplementations[_newImpl] == true, "implementation not exists.");
+        _availableImplementations[_newImpl] = false;
+    }
+    
+    /**
      * Only manager can change implementation of the module.
-     */ 
+     */
     function changeImplementation(address _module, address _newImpl) onlyManager public {
-        require(_module != 0x0);
-        require(_newImpl != 0x0);
+        require(_module != 0x0, "module not exists.");
+        require(_newImpl != 0x0, "implementation not exists.");
+        require(_availableImplementations[_newImpl] == true, "implementation not exists.");
         ModuleA(_module).changeImplementation(_newImpl);
+    }
+    
+    /**
+     * Only owner can update to the current implementation (the newest version).
+     */
+    function updateImplementation(address _module) public {
+        require(_module != 0x0, "module not exists.");
+        require(ModuleA(_module).getOwner() == msg.sender, "only owner can do it.");
+        ModuleA(_module).changeImplementation(_implementation);
     }
 }
