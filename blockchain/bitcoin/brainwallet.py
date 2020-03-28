f62f9b6fa8366d07b21fa871453c9ffa22ff39aa
Evan Liu <hmisty@gmail.com>
2020-03-28 13:48:32 +0800

    py3 compatible

--- blockchain/bitcoin/brainwallet.py
+++ blockchain/bitcoin/brainwallet.py
@@ -7,23 +7,23 @@ myBrainWalletPassword = getpass('password: ')
 again = getpass('password again: ')
 
 if not myBrainWalletPassword == again:
-    print 'mismatched passwords. quit...'
+    print('mismatched passwords. quit...')
 else:    
     mySecretKey = encode_privkey(sha256(myBrainWalletPassword), "wif")
     myAddress = pubtoaddr(privtopub(mySecretKey))
-    print
-    print 'secretKey:', '\033[37;47m' + mySecretKey + '\033[0m'
-    print '  address:', myAddress
+    print('\n')
+    print('secretKey:', '\033[37;47m' + mySecretKey + '\033[0m')
+    print('  address:', myAddress)
 
-    print
-    print 'verifying by signing... ', 
+    print('\n')
+    print('verifying by signing... ',) 
     test_msg = 'hello world'
     privkey = sha256(myBrainWalletPassword)
     sig = ecdsa_sign(test_msg, privkey)
     ownership = ecdsa_verify(test_msg, sig, myAddress)
 
     if ownership:
-        print 'passed.'
+        print('passed.')
     else:
-        print 'NOT passed. !!! WARNING !!!'
+        print('NOT passed. !!! WARNING !!!')
 
