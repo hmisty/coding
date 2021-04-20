3c04cde2a128e31e656880379cabe3b847f9306c
hmisty <hmisty@gmail.com>
2021-04-20 11:55:19 +0800

    remove greeter

--- blockchain/ethereum/hardhat_sample/scripts/deploy.js
+++ blockchain/ethereum/hardhat_sample/scripts/deploy.js
@@ -1,8 +1,8 @@
 async function main() {
-	const Greeter = await ethers.getContractFactory("Greeter");
-	const greeter = await Greeter.deploy("Hello, hardhat!");
+	const CSNFT = await ethers.getContractFactory("CryptoScientists");
+	const csnft = await CSNFT.deploy("Hello, Crypto Scientists!");
 
-	console.log("Greeter deployed to:", greeter.address);
+	console.log("Crypto Scientists NFT deployed to:", csnft.address);
 }
 
 main()
