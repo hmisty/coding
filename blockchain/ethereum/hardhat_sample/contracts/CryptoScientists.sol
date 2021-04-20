c9d6f480268e50e20a416560af831a125d21cebb
Evan Liu <evanliuchina@gmail.com>
2021-04-20 10:38:05 +0800

    crypto scientists

---
+++ blockchain/ethereum/hardhat_sample/contracts/CryptoScientists.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract CryptoScientists is ERC721 {
+    address _minter;
+    uint256 public __token_id = 0; // next id
+    mapping(uint256 => string) _token_hash; // ipfs hash for token[id]
+    mapping(string => bool) _hash_exists;
+
+    modifier onlyMinter {
+        require(_minter == msg.sender, "not minter");
+        _;
+    }
+
+    constructor() ERC721("Crypto Scientists NFT", "CSNFT") {
+        _minter = msg.sender;
+    }
+
+    function changeMinter(address m) public onlyMinter {
+        _minter = m;
+    }
+
+    function mint(string memory hash) public onlyMinter returns (uint256) {
+        require(_hash_exists[hash] == false, "already minted");
+        _hash_exists[hash] = true;
+
+        uint256 id = __token_id;
+        __token_id += 1;
+
+        _token_hash[id] = hash;
+        _safeMint(msg.sender, id);
+
+        return id;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://ipfs/";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "nonexistent token");
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0
+            ? string(abi.encodePacked(baseURI, _token_hash[tokenId]))
+            : '';
+    }
+
+
+
+}
