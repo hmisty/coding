c7bc9b778b8cf069d0e7742994544f12943d467e
hmisty <hmisty@gmail.com>
2021-04-20 14:41:22 +0800

    deploy to localhost ok

--- blockchain/ethereum/hardhat_sample/scripts/deploy.js
+++ blockchain/ethereum/hardhat_sample/scripts/deploy.js
@@ -1,6 +1,11 @@
 async function main() {
+	const [deployer] = await ethers.getSigners();
+
+	console.log("Deploying contracts with the account:", deployer.address);
+	console.log("Account balance:", (await deployer.getBalance()).toString());
+
 	const CSNFT = await ethers.getContractFactory("CryptoScientists");
-	const csnft = await CSNFT.deploy("Hello, Crypto Scientists!");
+	const csnft = await CSNFT.deploy();
 
 	console.log("Crypto Scientists NFT deployed to:", csnft.address);
 }
