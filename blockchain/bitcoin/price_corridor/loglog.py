1c5c47ffb55ed798b626fd510d56cf774ae9c73e
Evan Liu <evanliuchina@gmail.com>
2021-07-23 17:50:03 +0800

    draw

--- blockchain/bitcoin/price_corridor/loglog.py
+++ blockchain/bitcoin/price_corridor/loglog.py
@@ -3,18 +3,43 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter, FuncFormatter
 import datetime
 
+genesis = datetime.date(2009, 1, 3)
+
+#--- prepare data ---
+datafile = 'data/btc_history.csv'
+f = open(datafile, 'r')
+f.readline() # throw away the head line
+xs = []
+ys = []
+for line in f:
+    a, b = line.rstrip().split(',')
+    dt = datetime.datetime.strptime(a, '%Y/%m/%d')
+    x = (dt.date() - genesis).days
+    y = float(b)
+    xs.append(x)
+    ys.append(y)
+
+xs.reverse()
+ys.reverse()
+
+#--- prepare figure ---
+
+min_x = 1000
+max_x = 5000
+min_y = 1
+max_y = 100000
+
 fig, ax = plt.subplots(figsize=(16,9))
-ax.axis([500, 10000, 1, 1000000])
+ax.axis([min_x, max_x, min_y, max_y])
 
 ax.set_yscale('log')
 formatter_y = ScalarFormatter()
 formatter_y.set_scientific(False)
 ax.get_yaxis().set_major_formatter(formatter_y)
 
-genesis = datetime.date(2009, 1, 3)
 ticks_x = []
 
-for i in range(500, 10000):
+for i in range(min_x, max_x):
     date = genesis + datetime.timedelta(i)
     if date.month == date.day == 1:
         ticks_x.append(i)
@@ -29,7 +54,11 @@ def years(day, pos=None):
 formatter_x = FuncFormatter(years)
 ax.get_xaxis().set_major_formatter(formatter_x)
 
+ax.grid(True, linestyle='dotted')
+ax.grid(True, which='minor', axis='y', linestyle='dotted')
+
 plt.xticks(rotation=90)
+plt.plot(xs, ys)
 plt.show()
 
 #plt.savefig("out.png")
