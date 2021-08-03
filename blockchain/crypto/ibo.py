332ff86c2b56efd171f392e79745178cb7844abe
Evan Liu <evanliuchina@gmail.com>
2021-08-03 16:23:50 +0800

    ibo test

---
+++ blockchain/crypto/ibo.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+DECIMALS = 3
+
+symbolA = input('[?]Symbol A: ')
+symbolB = input('[?]Symbol B: ')
+symbolAB = symbolA + '/' + symbolB
+
+
+amountA = float(input('[?]Total amount of ' + symbolA + ' to be sold: '))
+conn_weight = float(input('[?]Connected Weight(0-1): '))
+initial_price = float(input('[?]Initial price of ' + symbolAB + ': '))
+
+supplyA = initialA = 1000 # just for initial pricing
+balanceB = initialB = round(initial_price * supplyA * conn_weight, DECIMALS)
+
+print()
+
+if not 0 <= conn_weight <= 1:
+    print('[!]Invalid CW.')
+else:
+    while True:
+        spot_price = round(balanceB / (supplyA * conn_weight), DECIMALS)
+        print('[=]Spot price of ' + symbolAB + ': ', spot_price)
+        print()
+
+        inB = float(input('[?]Input amount of ' + symbolB + ' (0 to quit): '))
+
+        if inB > 0:
+            outA = round(supplyA * ((1 + inB/balanceB)**conn_weight - 1), DECIMALS)
+
+            if amountA < outA:
+                print('[!]Not enough liquidity for ' + symbolA, '(', amountA, '<', outA, ')')
+            else:
+                print('[=]Output amount of ' + symbolA + ': ', outA)
+                print('[=]Effective price of ' + symbolAB + ': ', round(inB/outA, DECIMALS))
+
+                supplyA = round(supplyA + outA, DECIMALS)
+                amountA = round(amountA - outA, DECIMALS)
+                balanceB = round(balanceB + inB, DECIMALS)
+
+                print('[=]Sold ' + symbolA + ': ', supplyA - initialA)
+                print('[=]Remaining ' + symbolA + ': ', amountA)
+                print('[=]Raised ' + symbolB + ': ', balanceB - initialB)
+
+                if amountA == 0:
+                    print()
+                    print('Sold out.')
+                    break
+
+            print()
+        else:
+            break
+            
--- blockchain/crypto/readme.md
+++ blockchain/crypto/readme.md
@@ -33,3 +33,27 @@ and it can be directly unzipped.
 $ 7z e -o/path/to/directory filename.jpg
 ```
 
+
+## Bancor
+
+pay token B (connected token = base token) to buy token A (smart token = token on sale),\
+i.e. swap token B for token A,\
+i.e. IBO of A, where IBO is short for Initial Bancor Offering.
+
+assume:
+
+CW = Connected Weight, 0-100%
+
+pricing:
+
+```
+spot price = balance(B) / (supply(A) * CW)
+```
+
+swap:
+
+```
+out(A) = supply(A) * ((1 + in(B)/balance(B))^CW - 1)
+```
+
+
