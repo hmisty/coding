e1a89365e01526ac485f729564ed19839bd3fd32
Evan Liu <evanliuchina@gmail.com>
2021-08-05 15:52:50 +0800

    bancor formula

---
+++ blockchain/ethereum/bancor/test/sample-test.js
@@ -0,0 +1,33 @@
+const { expect } = require("chai");
+
+describe("Bancor", function() {
+	let formula;
+
+	beforeEach(async function() {
+    const deployer = await ethers.getContractFactory("BancorFormula");
+    formula = await deployer.deploy();
+    
+    await formula.deployed();
+	});
+
+  it("purchaseTargetAmount", async function() {
+		const supply = 1000;
+		const reserveBalance = 250;
+		const reserveWeight = 500000;
+		const amount = 10;
+		const purchased = await formula.purchaseTargetAmount(supply, reserveBalance, reserveWeight, amount);
+    expect(purchased).to.equal(19);
+
+  });
+
+  it("saleTargetAmount", async function() {
+		const supply = 1000;
+		const reserveBalance = 250;
+		const reserveWeight = 500000;
+		const amount = 1000;
+		const purchased = await formula.saleTargetAmount(supply, reserveBalance, reserveWeight, amount);
+    expect(purchased).to.equal(10);
+
+  });
+
+});
