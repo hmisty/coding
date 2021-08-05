cbab32afbdc11e92fb18bb1697ffc7431b5562c4
hmisty <hmisty@gmail.com>
2021-08-05 16:04:50 +0800

    update

--- blockchain/ethereum/bancor/test/sample-test.js
+++ blockchain/ethereum/bancor/test/sample-test.js
@@ -1,31 +1,37 @@
 const { expect } = require("chai");
 
 describe("Bancor", function() {
-    let formula;
+	let formula;
 
-    beforeEach(async function() {
-        const deployer = await ethers.getContractFactory("BancorFormula");
-        formula = await deployer.deploy();
+	beforeEach(async function() {
+		const deployer = await ethers.getContractFactory("BancorFormula");
+		formula = await deployer.deploy();
 
-        await formula.deployed();
-    });
+		await formula.deployed();
+	});
 
-    it("purchaseTargetAmount", async function() {
-        const supply = 1000;
-        const reserveBalance = 250;
-        const reserveWeight = 500000;
-        const amount = 10;
-        const purchased = await formula.purchaseTargetAmount(supply, reserveBalance, reserveWeight, amount);
-        expect(purchased).to.equal(19);
-    });
+	it("purchaseTargetAmount", async function() {
+		const supply = 1000;
+		const reserveBalance = 250;
+		const reserveWeight = 500000;
+		const amount = 10;
+		const purchased = await formula.purchaseTargetAmount(supply, reserveBalance, reserveWeight, amount);
+		expect(purchased).to.equal(19);
 
-    it("fundCost", async function() {
-        const supply = 1000;
-        const reserveBalance = 250;
-        const reserveWeight = 500000;
-        const amount = 19;
-        const purchased = await formula.fundCost(supply, reserveBalance, reserveWeight, amount);
-        expect(purchased).to.equal(10);
-    });
+		const purchased2 = await formula.purchaseTargetAmount(supply, reserveBalance, reserveWeight, 1000);
+		expect(purchased2).to.equal(1236);
+	});
+
+	it("fundCost", async function() {
+		const supply = 1000;
+		const reserveBalance = 250;
+		const reserveWeight = 500000;
+		const amount = 19;
+		const cost = await formula.fundCost(supply, reserveBalance, reserveWeight, amount);
+		expect(cost).to.equal(10);
+
+		const cost2 = await formula.fundCost(supply, reserveBalance, reserveWeight, 1236);
+		expect(cost2).to.equal(1000);
+	});
 
 });
