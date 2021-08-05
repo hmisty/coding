b45551f3fa04578722fd4595d85ad3b02018eb3b
hmisty <hmisty@gmail.com>
2021-08-05 16:00:35 +0800

    update

--- blockchain/ethereum/bancor/test/sample-test.js
+++ blockchain/ethereum/bancor/test/sample-test.js
@@ -1,33 +1,31 @@
 const { expect } = require("chai");
 
 describe("Bancor", function() {
-	let formula;
+    let formula;
 
-	beforeEach(async function() {
-    const deployer = await ethers.getContractFactory("BancorFormula");
-    formula = await deployer.deploy();
-    
-    await formula.deployed();
-	});
+    beforeEach(async function() {
+        const deployer = await ethers.getContractFactory("BancorFormula");
+        formula = await deployer.deploy();
 
-  it("purchaseTargetAmount", async function() {
-		const supply = 1000;
-		const reserveBalance = 250;
-		const reserveWeight = 500000;
-		const amount = 10;
-		const purchased = await formula.purchaseTargetAmount(supply, reserveBalance, reserveWeight, amount);
-    expect(purchased).to.equal(19);
+        await formula.deployed();
+    });
 
-  });
+    it("purchaseTargetAmount", async function() {
+        const supply = 1000;
+        const reserveBalance = 250;
+        const reserveWeight = 500000;
+        const amount = 10;
+        const purchased = await formula.purchaseTargetAmount(supply, reserveBalance, reserveWeight, amount);
+        expect(purchased).to.equal(19);
+    });
 
-  it("saleTargetAmount", async function() {
-		const supply = 1000;
-		const reserveBalance = 250;
-		const reserveWeight = 500000;
-		const amount = 1000;
-		const purchased = await formula.saleTargetAmount(supply, reserveBalance, reserveWeight, amount);
-    expect(purchased).to.equal(10);
-
-  });
+    it("fundCost", async function() {
+        const supply = 1000;
+        const reserveBalance = 250;
+        const reserveWeight = 500000;
+        const amount = 19;
+        const purchased = await formula.fundCost(supply, reserveBalance, reserveWeight, amount);
+        expect(purchased).to.equal(10);
+    });
 
 });
