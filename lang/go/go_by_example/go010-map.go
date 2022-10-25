0adc553972f17e5d7dc4388042b533b03e9e7c18
hmisty <hmisty@gmail.com>
2022-10-25 18:04:28 +0800

    add

--- lang/go/go_by_example/go010-map.go
+++ lang/go/go_by_example/go010-map.go
@@ -11,13 +11,17 @@ func main() {
 
 	fmt.Println("map:", m)
 
-	v1 := m["k1"]
-	fmt.Println("v1: ", v1)
+	v1, prs := m["k1"]
+	fmt.Println("v1: ", v1, "prs:", prs)
 	fmt.Println("len:", len(m))
 
 	delete(m, "k2")
 	fmt.Println("map:", m)
 
-	_, prs := m["k2"]
-	fmt.Println("prs:", prs)
+	v2, prs := m["k2"]
+	fmt.Println("v2:", v2, "prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
 }
